4984a5a7135d879c7e0cd45a41217eb5503e5df7
Patrick Breen <pressure78@gmail.com>
2019-10-03 13:24:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: <h1>dang</h1>,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log("doink!");
+    this.setState({
+      input: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App"> 
+        <textarea id="editor" value={this.state.input} onChange={this.handleChange} />
+        <div id="preview">{this.state.input}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
